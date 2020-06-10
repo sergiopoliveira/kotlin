27cf4afddac5910ab03d744b1fd05e7bffcbbd30
sergiopoliveira <sergiopoliveira@outlook.com>
2020-06-10 21:25:45 +0100

    Variables

--- src/intro/HelloWorld.kt
+++ src/intro/HelloWorld.kt
@@ -5,7 +5,14 @@ fun main() {
     println("Hello, $name!")
 }
 
+val question = "life, the universe " + "and everything"
+
 fun main(args: Array<String>) {
     val name = if (args.isNotEmpty()) args[0] else "Kotlin"
     println("Hello, $name!")
+    println(question)
+
+    var number = 1;
+    number++
+    println(number)
 }
